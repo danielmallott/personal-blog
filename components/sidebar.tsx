03c4f09301a15778ab73ff582fff824bd1c6a169
Dan Mallott <daniel.mallott@gmail.com>
2021-04-30 14:39:29 -0500

    Remove email and add Github

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Row, Image } from "react-bootstrap";
+import { Row } from 'react-bootstrap';
 
 function Sidebar() {
   return(
@@ -11,7 +11,7 @@ function Sidebar() {
         <h4>Connect with Me!</h4>
         <h5><i className="fab fa-twitter-square"></i><span className="ms-2"><a href="https://twitter.com/danielmallott" target="_blank">Twitter</a></span></h5>
         <h5><i className="fab fa-linkedin"></i><span className="ms-2"><a href="https://linkedin.com/in/danielmallott" target="_blank">LinkedIn</a></span></h5>
-        <h5><i className="far fa-envelope-square"></i><span className="ms-2"><a href="mailto:daniel.mallott@gmail.com">Email</a></span></h5>
+        <h5><i className="fab fa-github"></i><span className="ms-2"><a href="https://github.com/danielmallott/">Github</a></span></h5>
       </Row>
     </>
   );
