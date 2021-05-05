fcce177b7777b08a2e59f0d93010954a5c03f782
Dan Mallott <daniel.mallott@gmail.com>
2021-05-05 14:03:22 -0500

    Adding Twitter feed to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import { Row } from 'react-bootstrap';
+import { Timeline } from 'react-twitter-widgets';
 
 function Sidebar() {
   return(
@@ -8,11 +9,25 @@ function Sidebar() {
         <hr/>
       </Row>
       <Row>
+        <h4>My Latest Thoughts</h4>
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'danielmallott'
+          }}
+          options={{
+            height: '400'
+          }}
+          />
+      </Row>
+      <hr/>
+      <Row>
         <h4>Connect with Me!</h4>
         <h5><i className="fab fa-twitter-square"></i><span className="ms-2"><a href="https://twitter.com/danielmallott" target="_blank">Twitter</a></span></h5>
         <h5><i className="fab fa-linkedin"></i><span className="ms-2"><a href="https://linkedin.com/in/danielmallott" target="_blank">LinkedIn</a></span></h5>
         <h5><i className="fab fa-github"></i><span className="ms-2"><a href="https://github.com/danielmallott/">Github</a></span></h5>
       </Row>
+
     </>
   );
 }
