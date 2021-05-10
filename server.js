0a2aac42762db86707e71d1a0129c14817fb8761
Dan Mallott <daniel.mallott@gmail.com>
2021-05-10 10:11:34 -0500

    Adding Node server

---
+++ server.js
@@ -0,0 +1,77 @@
+const next = require('next')
+const { createServer } = require('http')
+const appInsights = require('applicationInsights')
+
+const initAppInsights = (instrumentationKey) => {
+  if (!instrumentationKey) {
+    return false;
+  }
+
+  appInsights
+    .setup(instrumentationKey)
+    .setAutoCollectConsole(true, true)
+    .setSendLiveMetrics(true)
+    .start();
+
+  return true;
+}
+
+const startServer = async (config) => {
+  const serverOptions = {
+    dev: config.env === 'development',
+    dir: '.',
+    quiet: false
+  };
+
+  const app = next(serverOptions);
+  const handleNextRequests = app.getRequestHandler();
+
+  const srv = createServer((req, res) => {
+    if (config.useAppInsights) {
+      appInsights.defaultClient.trackNodeHttpRequest({
+        request: req,
+        response: res
+      });
+    }
+
+    handleNextRequests(req, res);
+  });
+
+  await new Promise((resolve, reject) => {
+    srv.on('error', reject);
+    srv.on('listening', () => resolve());
+    srv.listen(config.port, config.hostname);
+  });
+
+  return app;
+}
+
+const startTime = Date.now();
+
+const serverConfig = {
+  hostname: 'localhost',
+  port: process.env.PORT || 3000,
+  env: process.env.APP_ENV || process.env.NODE_ENV || 'production',
+  useAppInsights: initAppInsights(process.env.NEXT_PUBLIC_APPINSIGHTS_INSTRUMENTATIONKEY)
+}
+
+startServer(serverConfig)
+  .then(async (app) => {
+    console.log(`started server on host ${serverConfig.hostname}, port ${serverConfig.port}, env ${serverConfig.env}`);
+
+    if (serverConfig.useAppInsights) {
+      const duration = Date.now() - startTime
+
+      appInsights.defaultClient.trackMetric({
+        name: 'server startup time',
+        value: duration
+      });
+    }
+
+    await app.prepare();
+  })
+  .catch((err) => {
+    console.error(err);
+
+    process.exit(1);
+  });
