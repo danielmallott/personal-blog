4ad18548aefd584cadfcfb3207db2b404ec2ecec
Dan Mallott <daniel.mallott@gmail.com>
2021-12-09 10:19:59 -0600

    Fixing broken build from Next 12.0.5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/dist/pages/_document';
 
 class CustomDocument extends Document {
   render () {
